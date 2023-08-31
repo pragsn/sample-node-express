b28755748d8132c3fc3126b64811c4f5be9503aa
pragsn <21pa1a04c6@vishnu.edu.in>
2023-08-31 10:47:02 -0700

    added to the application

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('hi');
+const express = require('express')
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+  res.send('Hello there i am prajna')
+})
+
+app.get('/hello', (req, res) => {
+    res.send('Hello there i am prajna')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
