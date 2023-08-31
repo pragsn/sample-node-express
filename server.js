0e6d06548674705e972cd3493b4395604cbbcf9e
pragsn <21pa1a04c6@vishnu.edu.in>
2023-08-31 11:24:27 -0700

    initial commit

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3003
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.send('Hello there i am prajna')
 })
 
-app.get('/hello', (req, res) => {
-    res.send('Hello there i am prajna')
+app.get('/signup', (req, res) => {
+    res.render('signup')
+  })
+
+  app.get('/signin', (req, res) => {
+    res.send('Hello there i am in signin page')
   })
 
 app.listen(port, () => {
