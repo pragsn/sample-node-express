a8dd38afc23803e7646d74d8a6f76ab053838c3b
pragsn <21pa1a04c6@vishnu.edu.in>
2023-09-10 06:02:44 -0700

    added express to the application

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3003
 
+const {initializeApp , cert} = require('firebase-admin/app');
+const { getFirestore }  = require('firebase-admin/firestore');
+
+
+var serviceAccount = require('./key.json');
+
+initializeApp({
+  credential: cert(serviceAccount),
+})
+
+const db = getFirestore();
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -11,9 +23,26 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
     res.render('signup')
   })
+  app.get('/signupsubmit', (req, res) => {
+  
+    const fullname = req.query.fullname;
+    const lastname = req.query.lastname;
+    const email = req.query.email;
+    const password = req.query.password;
 
-  app.get('/signin', (req, res) => {
-    res.send('Hello there i am in signin page')
+
+  db.collection('users').add({
+    name: fullname + lastname,
+    email: email,
+    password: password,
+  }).then(()=>{
+    res.send("signedup successfully")
+  });
+
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login')
   })
 
 app.listen(port, () => {
