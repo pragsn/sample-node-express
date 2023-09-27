aeb8f3261d99a386ed1012988287414fef306cf3
pragsn <21pa1a04c6@vishnu.edu.in>
2023-09-27 04:17:30 -0700

    login-signup-home

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const axios = require('axios');
+const app = express();
 const port = 3003
-
+var passwordHash = require('password-hash');
+var bodyParser = require('body-parser');
 const {initializeApp , cert} = require('firebase-admin/app');
 const { getFirestore }  = require('firebase-admin/firestore');
 
@@ -14,58 +16,108 @@ initializeApp({
 
 const db = getFirestore();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hello there i am prajna')
-})
+
+app.use(express.static('public'));
+
+
+
+app.post('/home', async (req, res) => {
+  const apiKey = '8d90a3a1da5db908fade0efae0e9773e'; 
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+
+
+  try {
+      const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric');
+      const weatherData = response.data;
+    
+      res.render('home', {
+          location: `Latitude: ${latitude}, Longitude: ${longitude}`,
+          temperature: weatherData.main.temp,
+          weatherDescription: weatherData.weather[0].description,
+
+      })
+  } catch (error) {
+      console.error(error);
+      res.send('Error fetching weather data.');
+  }
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+});
 
 app.get('/signup', (req, res) => {
     res.render('signup')
   })
-app.get('/loginsubmit', (req,res) =>{
-  const email = req.query.email;
-  const password = req.query.password;
-
+app.post('/loginsubmit', (req,res) =>{
+  const email = req.body.email;
+  const password = req.body.password;
+  //passwordHash.verify('password',)
   db.collection('users')
     .where('email', "==" ,email)
-    .where('password', "==" , password)
+    
     .get()
-    .then((docs) => {
-      if (docs.size>0){
-        var usersData=[];
-        db.collection('users').get().then(()=>{
-          docs.forEach((doc)=>{
-           usersData.push(doc.data());
-          });
-        })
-        .then(()=>{
-        console.log(usersData);
-        res.render('home', {usersData: usersData})
-        })
+    .then((docs)=>{
+      let verified = false;
+
+      docs.forEach(doc=>{
+        verified = passwordHash.verify(password,doc.data().password);
+        console.log(doc.id, "=>" , doc.data());
+      })
+      console.log(docs);
+      if(verified){
+        res.render('home');
       }
       else{
         res.send("login failed");
       }
+
+       //if(docs.size>0){
+        //res.render('home')
+      // }
+      //else{
+        //res.send("login failed");
+      //}
       
     });
 });
-  app.get('/signupsubmit', (req, res) => {
+  app.post('/signupsubmit', (req, res) => {
+    
+    console.log(req.body);
   
-    const fullname = req.query.fullname;
-    const lastname = req.query.lastname;
-    const email = req.query.email;
-    const password = req.query.password;
+    const fullname = req.body.fullname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password;
+    
+  db.collection("users")
+  .where("email" ,"==",email)
 
+  .get()
+  .then((docs)=>{
+    if (docs.size>0){
+      res.send("account already exists!")
+    }
+    else{
+      db.collection('users').add({
+        name: fullname + lastname,
+        email: email,
+        password:passwordHash.generate(password),
+      })
+      .then(()=>{
+        res.render('login')
+      });
+      
+    }
 
-  db.collection('users').add({
-    name: fullname + lastname,
-    email: email,
-    password: password,
-  }).then(()=>{
-    res.render('login')
-  });
-
+  })
   });
 
   app.get('/login', (req, res) => {
